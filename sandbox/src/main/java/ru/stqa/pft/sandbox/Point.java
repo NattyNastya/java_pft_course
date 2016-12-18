304fcffe728d564cda2fd2fbefef43c587205b89
NattyNastya <a.khamitsevich@gmail.com>
2016-12-18 19:07:26 +0300

    Created Poin class

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Z51-70 on 18.12.2016.
+ */
+public class Point {
+  public double x1;
+  public double y1;
+  public double x2;
+  public double y2;
+}

bbcdfb85cd3459cfb4d00ce0e8f3b57e61be4b05
NattyNastya <a.khamitsevich@gmail.com>
2016-12-19 01:32:25 +0300

    4th point of Homework 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenCoordinates.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenCoordinates.java
@@ -6,18 +6,14 @@ package ru.stqa.pft.sandbox;
 public class DistanceBetweenCoordinates {
 
   public static void main(String[] args) {
+    Point p1 = new Point(2, 2);
+    Point p2 = new Point(4, 15);
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x = 5;
-    p1.y = 6;
-    p2.x = 7;
-    p2.x = 8;
+    System.out.println("The distance between " + p1.x + ", " + p1.y + " and " + p2.x + ", " + p2.y + " coordinates is " + p1.distance(p2));
 
-    System.out.println("The distance between coordinates is " + distance(p1, p2));
   }
 
-  public static double distance (Point p1, Point p2){
+  /*public static double distance (Point p1, Point p2){
     return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
+  }*/
 }

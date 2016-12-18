95ee2b326d49d61e256577a9247db02048fd98de
NattyNastya <a.khamitsevich@gmail.com>
2016-12-18 20:14:20 +0300

    1 and 2 points of Homework 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,16 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 
+    Point p1 = new Point();
+    p1.x = 2;
+    p1.y = 2;
+
+    Point p2 = new Point();
+    p2.x = 4;
+    p2.y = 4;
+
+    System.out.println("The distance between points is " + distance(p1, p2));
+
 		hello("world");
     hello("user");
     hello("Nastya");
@@ -17,6 +27,10 @@ public class MyFirstProgram {
     //r.b = 8;
     System.out.println("The area of a rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 	}
+
+  public static double distance (Point p1, Point p2){
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,8 +4,6 @@ package ru.stqa.pft.sandbox;
  * Created by Z51-70 on 18.12.2016.
  */
 public class Point {
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
 }

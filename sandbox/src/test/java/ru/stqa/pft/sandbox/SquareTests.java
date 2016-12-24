5d085b54aae946075943c65f2b1444754947ea0b
NattyNastya <a.khamitsevich@gmail.com>
2016-12-25 02:38:05 +0300

    Added some additional conditions to the Square's test

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -15,5 +15,13 @@ public class SquareTests {
 
     Assert.assertEquals(s.area(), 25.0); // Now expected result will be displayed
 
+/*    double x = 24;
+
+    if ((x - s.area()) <= 1 && (x - s.area()) > 0) { //accept test if Actual result greater then Expected result for 1
+      System.out.println("Test passed");
+      System.out.println(x + " - " + s.area() + " = " + (x - s.area()));
+      System.out.println("Actual result = " + s.area());
+    } else Assert.assertEquals(s.area(), x);*/
+
   }
 }

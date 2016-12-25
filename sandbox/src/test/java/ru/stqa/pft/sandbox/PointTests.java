4f1d23a0137cdd00c4d7ceb269bd623a999a7d74
NattyNastya <a.khamitsevich@gmail.com>
2016-12-25 20:34:25 +0300

    Added some annotations

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  * Created by Z51-70 on 25.12.2016.
@@ -20,6 +20,41 @@ public class PointTests {
     System.out.println(actualRes);                 // Rounding the actual result
     Assert.assertEquals(actualRes, 3.0);
 
-
   }
+
+  @Test
+  public void testCase2() {
+    System.out.println("This is Test Case 2");
+  }
+
+  @BeforeMethod
+  public void beforeMethod() {
+    System.out.println("This test will be executed Before every Method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+    System.out.println("This test will be executed After every Method");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+    System.out.println("This test will be executed Before every Class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+    System.out.println("This test will be executed After every Class");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+    System.out.println("Before test execution");
+  }
+
+  @AfterTest
+  public void afterTest() {
+    System.out.println("After Test execution");
+  }
+
 }

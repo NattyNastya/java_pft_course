225e8e2059fad181ec010d484404727f9c7667b3
NattyNastya <a.khamitsevich@gmail.com>
2017-02-05 23:19:43 +0300

    L4.5 - The formation of the Collections of the GroupData type
    Replaced checks by index to check by collection size

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
   @Test
@@ -13,12 +15,14 @@ public class GroupDeletionTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("TEST3", "TEST3", "TEST3"));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);//Group selection by index (now it is the last element)
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    // int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().selectGroup(before.size() - 1);//Group selection by index (now it is the last element)
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before -1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    // int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by Z51-70 on 09.01.2017.
  */
@@ -15,13 +17,15 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("TEST1", "TEST3", "TEST3"));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);//Group selection by index (now it is the last element)
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    // int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().selectGroup(before.size() - 1);//Group selection by index (now it is the last element)
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("The Surgeons", "test header (Logo)", "test footer (comment)"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(before, after);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    // int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(before.size(), after.size());
   }
 }
